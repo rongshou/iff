52f8699497f2bb9958123c9f51cd88aa7c89d859
admin <admin@tianquan.local>
2026-06-24 21:53:38 +0800

    remove Recommend and News pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import RecommendPage from "./pages/Recommend";
 import ExplorePage from "./pages/Explore";
-import NewsPage from "./pages/News";
 import ChatPage from "./pages/Chat";
 
 const BASE = import.meta.env.BASE_URL.replace(/\/$/, "");
@@ -11,8 +9,6 @@ export default function App() {
     <BrowserRouter basename={BASE}>
       <Routes>
         <Route path="/" element={<ChatPage />} />
-        <Route path="/recommend" element={<RecommendPage />} />
-        <Route path="/news" element={<NewsPage />} />
         <Route path="/explore" element={<ExplorePage />} />
         <Route path="/chat" element={<ChatPage />} />
       </Routes>
